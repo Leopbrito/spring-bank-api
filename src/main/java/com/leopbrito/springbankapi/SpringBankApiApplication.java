05add3492fe564827951c3eb4e141a61c0b67b62
Leonardo Brito <leopbrito@ibm.com>
2021-03-18 17:07:46 -0300

    Initial project

---
+++ src/main/java/com/leopbrito/springbankapi/SpringBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.leopbrito.springbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBankApiApplication.class, args);
+	}
+
+}
